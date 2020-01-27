edfad915e9aecd11f93b7d2aaa248ed93cd2eff0
realsdx <realsdx@protonmail.com>
2020-01-27 23:35:24 +0530

    add new field in transaction admin display

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from api.models import ApiUser, Transaction, Coupon
 
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ['amount','sender','receiver','msg']
+    list_display = ['amount','sender','receiver','created_at','msg']
 
 admin.site.register(ApiUser)
 admin.site.register(Transaction, TransactionAdmin)
