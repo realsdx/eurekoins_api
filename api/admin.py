467ac8b8883da84a3367d73023b04d14dab44b3f
realsdx <realsdx@protonmail.com>
2020-02-05 12:16:56 +0530

    Add search in admin

--- api/admin.py
+++ api/admin.py
@@ -3,12 +3,14 @@ from api.models import ApiUser, Transaction, Coupon
 
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ['amount','sender','receiver','created_at','msg']
+    search_fields = ['receiver', 'sender']
 
 class CouponAdmin(admin.ModelAdmin):
     list_display = ['code','amount','is_active']
 
 class ApiUserAdmin(admin.ModelAdmin):
     list_display = ['email','name','coins']
+    search_fields = ['email', 'name']
 
 admin.site.register(ApiUser, ApiUserAdmin)
 admin.site.register(Transaction, TransactionAdmin)
